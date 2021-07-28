8d7c44020dc128fe26b6b202d46616366909fcff
ramya <ramyaf976@gmail.com>
2021-07-28 07:27:25 +0530

    Committed

---
+++ source/src/source/food.java
@@ -0,0 +1,22 @@
+package source;
+
+public class food {
+	
+	String name;
+
+	public food(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
---
+++ source/src/source/mainclass.java
@@ -0,0 +1,10 @@
+package source;
+
+public class mainclass {
+	public static void main(String[] args) {
+		
+		food f= new food("Pasta");
+		System.out.println(f.getName());
+	}
+
+}
